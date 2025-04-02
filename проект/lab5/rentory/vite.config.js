30ae29380b28f3aa53dab2014a6d4faef48916a2
dargoosha <dariia.herasymchuk@knu.ua>
2025-04-02 12:25:44 +0300

    lab5

---
+++ проект/lab5/rentory/vite.config.js
@@ -0,0 +1,10 @@
+
+import { defineConfig} from "vite"; 
+import pugPlugin from "vite-plugin-pug";
+
+const options = { pretty: true }; 
+const locals = { name: "My Pug" };
+
+export default defineConfig({
+    plugins: [pugPlugin(undefined, { pagesUrl: "./pages/" })], 
+});
